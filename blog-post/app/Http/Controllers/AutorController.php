ab9a9607f0ef3dc13763b16e4f9f605883605703
Aleksa Djordjevic <aleksa.djordjevic2312@gmail.com>
2022-12-28 02:15:27 +0100

    Dodati API 6+ ruta, kontroleri

---
+++ blog-post/app/Http/Controllers/AutorController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\AutorCollection;
+use App\Http\Resources\AutorResource;
+use App\Models\Autor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AutorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $autors = Autor::all();
+        return $autors;
+
+      //  $autors=Autor::all();
+      //  return new AutorCollection($autors);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Autor  $autor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Autor $autor)
+    {
+        return new AutorResource($autor);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Autor  $autor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Autor $autor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Autor  $autor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Autor $autor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Autor  $autor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Autor $autor)
+    {
+        //
+    }
+}
--- blog-post/routes/api.php
+++ blog-post/routes/api.php
@@ -1,5 +1,15 @@
 <?php
-
+use App\Models\Knjiga;
+use App\Models\Autor;
+use App\Models\Zanr;
+use App\Models\User;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\KnjigaController;
+use App\Http\Controllers\AutorController;
+use App\Http\Controllers\ZanrController;
+use App\Http\Controllers\KnjigaAutorController;
+use App\Http\Controllers\KnjigaZanrController;
+use App\Http\Controllers\UserKnjigaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +27,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users', [UserController::class, 'index'])->name('users.show');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('users.index');
+//Route::resource('knjigas', KnjigaController::class);
+//Route::resource('user.knjigas', UserKnjigaController::class);
+Route::get('users/{id}/knjigas', [UserKnjigaController::class,'index'])->name('users.knjigas.index');
+Route::resource('knjiga', KnjigaController::class)->only(['index','show','update','store','destroy']);
+Route::resource('autor', AutorController::class);
+Route::resource('zanr', ZanrController::class);
+Route::resource('autor.knjiga', KnjigaAutorController::class);
+Route::resource('zanr.knjiga', KnjigaZanrController::class);
